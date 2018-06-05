c5152b25b6170751b499da7ff2b3ca8a3712cac4
Jakub Wlodek <jwlodek@debian>
2018-06-05 17:17:14 -0400

    fixed some header .h files

--- barApp/barSrc/NDPluginBar.h
+++ barApp/barSrc/NDPluginBar.h
@@ -8,6 +8,15 @@
 #define BAR_MODIFICATION 0
 
 //Here I will define all of the output data types once the database is written
+#define NDPluginBarBarcodeMessageString "BARCODE_MESSAGE" //waveform
+#define NDPluginBarBarcodeTypeString "BARCODE_TYPE" //waveform
+#define NDPluginBarNumberCodesString "NUMBER_CODES" //asynInt32
+#define NDPluginBarBacodeFoundString "BARCODE_FOUND" //asynInt32
+#define NDPluginBarUpperLeftString "UPPER_LEFT" //asynInt32
+#define NDPluginBarUpperRightString "UPPER_RIGHT" //asynInt32
+#define NDPluginBarLowerLeftString "LOWER_LEFT" //asynInt32
+#define NDPluginBarLowerLeftString "LOWER_LEFT" //asynInt32
+
 
 //class that does barcode readings
 class NDPluginBar : public NDPluginDriver {
@@ -21,7 +30,30 @@ class NDPluginBar : public NDPluginDriver {
 	protected:
 
 		//in this section, once i define the database values, I will have to define them here
+		//message contained in bar code
+		char* NDPluginBarBarcodeMessage;
 
+		//type of bar code i.e. QR, BAR
+		char* NDPluginBarBarcodeType;
+		
+		//if there is a bar code found
+		int NDPluginBarBarcodeFound;
+
+		//number of codes found
+		int NDPluginBarNumberCodes;
+
+		//upper left pixel of found bar code
+		int NDPluginBarUpperLeft;
+		
+		//upper right pixel of found bar code
+		int NDPluginBarUpperRight;
+
+		//lower left pixel of found bar code
+		int NDPluginBarLowerLeft;
+
+		//lower right pixel of found bar code
+		int NDPluginBarLowerRight;
+		
 	private:
 
 };
